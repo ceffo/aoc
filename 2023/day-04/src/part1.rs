7eaabab57628b35ca001a34f5c385128126c8e6c
Moncef Naji <moncef.naji@gmail.com>
2023-12-09 21:59:38 -0500

    feat(day-04): part1

--- 2023/day-04/src/part1.rs
+++ 2023/day-04/src/part1.rs
@@ -1,13 +1,105 @@
+use std::collections::HashSet;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::line_ending,
+    combinator::map,
+    multi::{many0, many1},
+    sequence::{preceded, terminated},
+    IResult,
+};
+
 use crate::custom_error::AocError;
 
+#[derive(Debug, PartialEq, Eq)]
+struct Card {
+    index: u32,
+    winning: HashSet<u32>,
+    have: HashSet<u32>,
+}
+
+impl Card {
+    fn new(index: u32, winning: HashSet<u32>, have: HashSet<u32>) -> Self {
+        Self {
+            index,
+            winning,
+            have,
+        }
+    }
+
+    fn score(&self) -> u32 {
+        let winning = self.winning.intersection(&self.have);
+        let num_winning_numbers = winning.count() as u32;
+        match num_winning_numbers {
+            0 => 0,
+            n => 2u32.pow(n - 1),
+        }
+    }
+}
+
+fn vec_to_set<T>(vec: Vec<T>) -> HashSet<T>
+where
+    T: Eq + std::hash::Hash,
+{
+    HashSet::from_iter(vec)
+}
+
+fn parse_number(input: &str) -> IResult<&str, u32> {
+    preceded(
+        nom::character::complete::space0,
+        nom::character::complete::u32,
+    )(input)
+}
+
+fn parse_number_set(input: &str) -> IResult<&str, HashSet<u32>> {
+    map(many1(parse_number), vec_to_set)(input)
+}
+
+// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+fn parse_card(input: &str) -> IResult<&str, Card> {
+    let (input, _) = tag("Card ")(input)?;
+    let (input, index) = parse_number(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, winning) = parse_number_set(input)?;
+    let (input, _) = tag(" |")(input)?;
+    let (input, have) = parse_number_set(input)?;
+
+    Ok((input, Card::new(index, winning, have)))
+}
+
+fn parse_cards(input: &str) -> IResult<&str, Vec<Card>> {
+    let mut parser = many0(terminated(parse_card, line_ending));
+    parser(input)
+}
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    Ok("wrong answer".to_string())
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let (_, cards) = parse_cards(input).map_err(|e| AocError::ParseError(e.to_string()))?;
+    let total_score = cards.iter().map(|card| card.score()).sum::<u32>();
+    Ok(total_score.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(Card::new(1, [1,2,3].into(), [].into()), 0)]
+    #[case(Card::new(1, [1,2,3].into(), [1].into()), 1)]
+    #[case(Card::new(1, [1,2,3].into(), [1,2].into()), 2)]
+    #[case(Card::new(1, [1,2,3].into(), [1,2,3].into()), 4)]
+    #[case(Card::new(1, [1,2,3,4,5,6,7].into(), [1,2,3,4].into()), 8)]
+    fn test_card_score(#[case] card: Card, #[case] expected: u32) {
+        assert_eq!(expected, card.score());
+    }
+
+    #[rstest]
+    #[case("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", Card::new(1, [41,48,83,86,17].into(), [83,86,6,31,17,9,48,53].into()))]
+    fn test_parse_card(#[case] input: &str, #[case] expected: Card) {
+        let (_, card) = parse_card(input).unwrap();
+        assert_eq!(expected, card);
+    }
 
     #[test]
     fn test_process() -> miette::Result<()> {
